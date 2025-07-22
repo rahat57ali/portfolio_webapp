f949c898973653f27c3f5b7811e36e633e048c47
Rahat <rahatalisoomro10@gmail.com>
2025-07-23 00:27:11 +0500

    Color Scheme changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GithubIcon, LinkedinIcon, InstagramIcon, TwitterIcon } from './Icons'; 
+import { GithubIcon, LinkedinIcon, InstagramIcon, TwitterIcon } from './Icons';
 
 const navLinks = [
     { id: 'about', title: 'About' },
@@ -18,16 +18,16 @@ function Header({ activeSection }) {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-2/5 lg:flex-col lg:justify-between lg:py-24">
       <div>
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Rahat Ali</h1>
-        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Junior Backend Developer</h2>
-        <p className="mt-4 max-w-xs leading-normal">I build accessible, pixel-perfect digital experiences for the web.</p>
+        <h1 className="text-4xl font-bold tracking-tight text-[#111439] sm:text-5xl">Rahat Ali</h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight text-[#111439] sm:text-xl">Junior Backend Developer</h2>
+        <p className="mt-4 max-w-xs leading-normal text-[#5B5F7E]">I build accessible, pixel-perfect digital experiences for the web.</p>
         <nav className="nav hidden lg:block" aria-label="In-page jump links">
           <ul className="mt-16 w-max">
             {navLinks.map(link => (
               <li key={link.id}>
                 <a className={`group flex items-center py-3 nav-link ${activeSection === link.id ? 'active' : ''}`} href={`#${link.id}`}>
-                  <span className="nav-indicator mr-4 h-px w-[30px] bg-slate-600 transition-all group-hover:w-[60px] group-hover:bg-slate-200 motion-reduce:transition-none"></span>
-                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200">{link.title}</span>
+                  <span className="nav-indicator mr-4 h-px w-[30px] bg-[#5B5F7E] transition-all group-hover:w-[60px] group-hover:bg-[#111439] motion-reduce:transition-none"></span>
+                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-[#5B5F7E] group-hover:text-[#111439]">{link.title}</span>
                 </a>
               </li>
             ))}
@@ -37,7 +37,7 @@ function Header({ activeSection }) {
       <ul className="ml-1 mt-8 flex items-center" aria-label="Social media">
         {socialLinks.map(social => (
             <li key={social.label} className="mr-5 text-xs">
-                <a href={social.url} target="_blank" rel="noreferrer noopener" aria-label={social.label} className="block text-slate-400 hover:text-slate-200 transition">
+                <a href={social.url} target="_blank" rel="noreferrer noopener" aria-label={social.label} className="block text-[#5B5F7E] hover:text-[#111439] transition">
                     <social.icon />
                 </a>
             </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,20 +5,20 @@ import { FolderIcon, ExternalLinkIcon } from './Icons';
 function Projects() {
   return (
     <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
-        <div className="sticky top-0 z-10 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"><h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2></div>
+        <div className="sticky top-0 z-10 -mx-6 mb-4 w-screen bg-[#f8f8f9]/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"><h2 className="text-sm font-bold uppercase tracking-widest text-[#111439] lg:sr-only">Projects</h2></div>
         <div>
             <ul className="group/list">
                 {projectsData.map((project, index) => (
                     <li key={index} className="mb-12">
                         <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                            <div className="absolute -inset-x-4 -inset-y-2.5 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                            <div className="absolute -inset-x-4 -inset-y-2.5 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-white/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                             <div className="z-10 sm:order-2 sm:col-span-6">
-                                <h3><a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-green-300 focus-visible:text-green-300 group/link text-base" href={project.link} target="_blank" rel="noreferrer"><span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span><span>{project.title}</span></a></h3>
+                                <h3><a className="inline-flex items-baseline font-medium leading-tight text-[#111439] hover:text-[#6772e5] focus-visible:text-[#6772e5] group/link text-base" href={project.link} target="_blank" rel="noreferrer"><span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span><span>{project.title}</span></a></h3>
                                 <p className="mt-2 text-sm leading-normal">{project.description}</p>
                                 {project.technologies.length > 0 && (
                                     <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
                                     {project.technologies.map((tech, i) => (
-                                        <li key={i} className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-green-400/10 px-3 py-1 text-xs font-medium leading-5 text-green-300">{tech}</div></li>
+                                        <li key={i} className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-[#6772e5]/10 px-3 py-1 text-xs font-medium leading-5 text-[#6772e5]">{tech}</div></li>
                                     ))}
                                     </ul>
                                 )}
@@ -30,27 +30,25 @@ function Projects() {
             </ul>
         </div>
         
-        <h2 className="mt-24 mb-4 text-lg font-semibold text-slate-200">Other Noteworthy Projects</h2>
+        <h2 className="mt-24 mb-4 text-lg font-semibold text-[#111439]">Other Noteworthy Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {noteworthyProjectsData.map((project, index) => (
-                <div key={index} className="p-6 rounded-md shadow-lg project-card">
-                    <div className="flex justify-between items-center mb-4">
+                <a href={project.link} target="_blank" rel="noreferrer" key={index} className="p-6 rounded-md shadow-lg project-card group">
+                    <div className="flex justify-between items-center mb-4 text-[#5B5F7E] group-hover:text-[#6772e5] transition-colors">
                         <FolderIcon />
-                        <a href={project.link} target="_blank" rel="noreferrer" className="text-slate-400 hover:text-green-300">
-                           <ExternalLinkIcon />
-                        </a>
+                        <ExternalLinkIcon />
                     </div>
-                    <h3 className="text-slate-200 font-semibold mb-2">{project.title}</h3>
-                    <p className="text-sm mb-4">{project.description}</p>
-                    <div className="flex flex-wrap gap-2 text-xs font-mono text-slate-400">
+                    <h3 className="text-[#111439] font-semibold mb-2">{project.title}</h3>
+                    <p className="text-sm mb-4 text-[#5B5F7E]">{project.description}</p>
+                    <div className="flex flex-wrap gap-2 text-xs font-mono text-[#5B5F7E]">
                         {project.technologies.map((tech, i) => (
                             <span key={i}>{tech}</span>
                         ))}
                     </div>
-                </div>
+                </a>
             ))}
         </div>
-        <div className="mt-12"><a className="group inline-flex items-center font-semibold leading-tight text-slate-200" aria-label="View Full Project Archive" href="/archive"><span><span className="border-b border-transparent pb-px transition group-hover:border-green-300 motion-reduce:transition-none">View Full Project Archive</span><span className="whitespace-nowrap"><span className="ml-1 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">→</span></span></span></a></div>
+        <div className="mt-12"><a className="group inline-flex items-center font-semibold leading-tight text-[#111439]" aria-label="View Full Project Archive" href="/archive"><span><span className="border-b border-transparent pb-px transition group-hover:border-[#6772e5] motion-reduce:transition-none">View Full Project Archive</span><span className="whitespace-nowrap"><span className="ml-1 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">→</span></span></span></a></div>
     </section>
   );
 }
