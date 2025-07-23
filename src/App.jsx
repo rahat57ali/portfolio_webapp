5efc654bc76fafbb4f127f6325c6e6f9f5accc98
Rahat <rahatalisoomro10@gmail.com>
2025-07-24 00:40:09 +0500

    Vercel analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   const [activeSection, setActiveSection] = useState('about');
